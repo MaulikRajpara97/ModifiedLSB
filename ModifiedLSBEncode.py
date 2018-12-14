a3ffa79c5f3fafa96b530ba509942f5ac41a4849
MaulikRajpara97 <rajparamaulik@gmail.com>
2018-12-13 20:09:24 -0600

    Add files via upload

---
+++ ModifiedLSBEncode.py
@@ -0,0 +1,33 @@
+
+import wave
+import os
+
+song = wave.open("4.wav", mode='rb')
+
+frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+song.setpos(0)
+finaltemp=''
+hidden_file = open('data.txt', 'r')
+
+file_size = os.path.getsize('data.txt')
+
+string = hidden_file.read(-1)
+print "Key=",len(string)
+
+
+sf = song.getnframes()  // len(string)
+for i in range(len(string)):
+    temp=bytearray(list(song.readframes(sf)))   
+    tempstring = string[i] + int((len(temp)-(len(string[i])*8*8))/8) *'&'    
+    tempbits = list(map(int, ''.join([bin(ord(j)).lstrip('0b').rjust(8,'0') for j in tempstring])))
+    for k, tempbit in enumerate(tempbits):
+        temp[k] = (temp[k] & 254) | tempbit
+    finaltemp=finaltemp+bytes(temp)
+    
+nd = wave.open('vaikunth.wav', 'wb')
+nd.setparams(song.getparams())
+nd.writeframes(finaltemp)
+print "Stego Audio Genrated"
+nd.close()
+
+song.close()
