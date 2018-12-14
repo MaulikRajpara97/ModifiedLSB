0bc3a02d19c514d3c6ca58e5ad720b1b677b90ed
MaulikRajpara97 <rajparamaulik@gmail.com>
2018-12-13 20:15:27 -0600

    Add files via upload

---
+++ ModifiedLSBDecode.py
@@ -0,0 +1,20 @@
+# Use wave package (native to Python) for reading the received audio file
+import wave
+song = wave.open("output.wav", mode='rb')
+# Convert audio to byte array
+frame_bytes = bytearray(list(song.readframes(song.getnframes())))
+DecodeMsg=''
+song.setpos(0)
+length=12
+sf = song.getnframes()  // length
+for i in range(length):
+    temp=bytearray(list(song.readframes(sf)))
+    tempextracted=[temp[i] & 1 for i in range(len(temp))]    
+    tempstring = "".join(chr(int("".join(map(str,tempextracted[i:i+8])),2)) for i in range(0,len(tempextracted),8))    
+    decoded = tempstring.split("&")[0]
+    DecodeMsg=DecodeMsg+decoded
+print "Secret Message :",DecodeMsg
+    
+
+
+song.close()
